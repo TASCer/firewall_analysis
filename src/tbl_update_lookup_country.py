d8ec57f75534ec50a33d73901268976e66e9f0ca
TASCer <todd@tascs.net>
2022-06-29 15:29:41 -0700

    -WHOIS functionality

--- src/tbl_update_lookup_country.py
+++ src/tbl_update_lookup_country.py
@@ -1,9 +1,8 @@
 # New entries into lookup table will have a NULL COUNTRY value
-# If source ip gets an ASN response, but no ALPHA-2 country code, '' is entered into lookup tbl
+# If source ip gets an ASN response, but no ALPHA-2 country code, NULL remains
 # If country code is found, but code not in countries table, its ALPHA-2 code is entered into lookup table to resolve later
 # Populated 'countries' table using file found on the web
 # I exported my countries' table to the sample folder
-
 
 import sqlalchemy as sa
 import ipwhois
@@ -17,52 +16,33 @@ engine = sa.create_engine("mysql+pymysql://{0}:{1}@{2}/{3}".format(my_secrets.db
 def update():
     """Updates lookup table with unique ips from ALPHA-2 to full country name"""
     with engine.connect() as conn, conn.begin():
-        sql = '''SELECT source, country from lookup WHERE country is Null or country = '';'''
+        sql = '''SELECT source, country from lookup WHERE country is Null;'''
         lookups = conn.execute(sql)
 
         for ip, country in lookups:
-
-            # Try to get a response via RDAP
+            # Try to get a response with country ALPHA2 via RDAP
             try:
                 obj = ipwhois.IPWhois(ip)
                 result = obj.lookup_rdap()
-
-            except (ipwhois.BaseIpwhoisException, ipwhois.ASNLookupError, ipwhois.ASNParseError, ipwhois.ASNOriginLookupError,
-                    ipwhois.ASNRegistryError, ipwhois.HostLookupError, ipwhois.HTTPLookupError) as e:
-                msg = str(e)
-                print(msg + f" RDAP EXCEPT on {ip}")
-
-            # If RDPA fails, try to get a result via WHOIS
-            if not result:
-
-                try:
-                    obj = ipwhois.IPWhois(ip)
-                    result = obj.lookup_whois()
-
-                except (ipwhois.BaseIpwhoisException, ipwhois.ASNLookupError, ipwhois.ASNParseError, ipwhois.ASNOriginLookupError,
-                        ipwhois.ASNRegistryError, ipwhois.HostLookupError, ipwhois.HTTPLookupError) as e:
-                    msg = str(e)
-                    print(msg + f" Whois EXCEPT on {ip}")
-
-            # Try to get the country code
-            try:
-                country_res = result['asn_country_code']
-                if country_res:
-                    country_res = country_res.lower()
-
-                elif country_res is None:
+                if result['asn_country_code'] == '':
                     raise ValueError
+                else:
+                    asn_alpha2 = result['asn_country_code'].lower()
+                    print('asn_alpha2', asn_alpha2)
 
-            except (ValueError, AttributeError) as e:
-                print(str(e), f"NO country code found in asn for {ip}")
+            except (ValueError, AttributeError, ipwhois.BaseIpwhoisException, ipwhois.ASNLookupError,
+                    ipwhois.ASNParseError, ipwhois.ASNOriginLookupError,
+                    ipwhois.ASNRegistryError, ipwhois.HostLookupError, ipwhois.HTTPLookupError) as e:
+                print(str(e) + f" RDAP EXCEPT on {ip}")
+                continue
 
             # Try to get country name from ALPHA-2 country code
             try:
-                country_res = result['asn_country_code'].lower()
-                country_lookup = conn.execute(f"SELECT name from countries WHERE alpha2 = '{country_res}';")
+                country_lookup = conn.execute(f"SELECT name from countries WHERE alpha2 = '{asn_alpha2}';")
                 country = [n for n in country_lookup][0][0]
                 conn.execute(f'''update lookup SET country = '{country}' WHERE SOURCE = '{ip}';''')
 
             except Exception as e:
-                print(str(e), f'***country name not found in countries DB for {country_res}. Research ISO codes*** ')
-                conn.execute(f'''update lookup SET country = '{country_res}' WHERE SOURCE = '{ip}';''')
+                print(str(e), f'***country name not found in countries DB for {ip}. Research*** ')
+                conn.execute(f'''update lookup SET country = '{asn_alpha2}' WHERE SOURCE = '{ip}';''')
+                print(f'asn_alpha2 {asn_alpha2} added for {ip}. Research Code*** ')
