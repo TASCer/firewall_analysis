628096531a483fea0d99f4344edd81afd32687db
TASCer <todd@tascs.net>
2022-07-22 11:12:04 -0700

    + try/except, logging

--- src/log_visual_analysis.py
+++ src/log_visual_analysis.py
@@ -1,15 +1,35 @@
-import pandas as pd
-import sqlalchemy as sa
+import logging
 import matplotlib.pyplot as plt
 import my_secrets
-from collections import Counter
+import pandas as pd
 
-engine = sa.create_engine("mysql+pymysql://{0}:{1}@{2}/{3}".format(my_secrets.dbuser, my_secrets.dbpass, my_secrets.dbhost, my_secrets.dbname))
+from collections import Counter
+from sqlalchemy import create_engine, exc, types
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('./log.log')
+fh.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
 
 
 # TODO Analyze current processed log. Tweak Country, add time span to all plots?
 def analyze(log):
 	"""Takes cureently processed log and presents information to screen and file"""
+	try:
+		engine = create_engine("mysql+pymysql://{0}:{1}@{2}/{3}".format(my_secrets.dbuser, my_secrets.dbpass,
+																		my_secrets.dbhost, my_secrets.dbname))
+
+	except exc.SQLAlchemyError as e:
+		logger.critical(str(e))
+		engine = None
+		exit()
+
 	freq_ports = log.groupby(['DPT']).size()
 	freq_ports_sorted = freq_ports.sort_values(ascending=False).head(15)
 	freq_hostnames = log.groupby(['HOSTNAME']).size()
@@ -26,11 +46,10 @@ def analyze(log):
 			get_country_list = list(get_country.index.values)
 			top_countries.append(get_country_list.pop())
 	counter_top_countries = Counter(top_countries)
-	counter_no_country_name = {k: v for k, v in counter_top_countries.items() if len(k) == 2 or v == 'notfound'}
+	counter_no_country_name = {k: v for k, v in counter_top_countries.items() if len(k) == 2 or v == 'notfound' or v == 'error'}
 	# no_countries = [k for k,v in counter_top_countries.values() if (len(counter_top_countries.values()) == 2)]
 	top_15_countries = counter_top_countries.most_common(15)
 
-	print(top_15_countries, type(top_15_countries))
 
 # Plot Top Countries
 	plt.style.use('ggplot')  # 'ggplot' 'classic'
