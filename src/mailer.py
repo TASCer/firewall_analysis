006d6e9b6a74d383af09cf51d0981529d5df64ae
TASCer <todd@tascs.net>
2022-07-22 11:29:42 -0700

    moved logger

--- src/mailer.py
+++ src/mailer.py
@@ -5,7 +5,7 @@ import smtplib
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('./log.log')
+fh = logging.FileHandler('../log.log')
 fh.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
